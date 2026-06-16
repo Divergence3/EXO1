19ab9899257f56e2a0c65c32be7bf59c2f44419f
Divergence3 <kayembed122@gmail.com>
2026-06-16 13:21:23 +0000

    Amelioration affichage

---
+++ exo1.py
@@ -0,0 +1,19 @@
+# guess_number.py
+
+import random
+
+number = random.randint(1, 100)
+
+print("=== GUESS THE NUMBER ===")
+print("Devine un nombre entre 1 et 100")
+
+while True:
+    guess = int(input("Votre proposition : "))
+
+    if guess < number:
+        print("Trop petit")
+    elif guess > number:
+        print("Trop grand")
+    else:
+        print("Bravo ! Vous avez trouvé.")
+        break
